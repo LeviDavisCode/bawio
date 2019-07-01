8bc77c9e84ebcde61101d8a9d07d06ab806325eb
LeviDavisCode <47396371+LeviDavisCode@users.noreply.github.com>
2019-07-01 16:02:20 +0100

    missing video fixed

--- player.js
+++ player.js
@@ -1,7 +1,9 @@
 function openPlay() {
   document.getElementById("player").style.height = "100%";
+  document.getElementById("player").style.width = "100%";
 }
 
 function closePlay() {
   document.getElementById("player").style.height = "0%";
+  document.getElementById("player").style.width = "0%";
 }
