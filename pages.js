2b1750d6dc0b4a510b1b430b6b031a60b255e822
LeviDavisCode <47396371+LeviDavisCode@users.noreply.github.com>
2019-07-01 23:05:50 +0100

    MORE PAGES

---
+++ pages.js
@@ -0,0 +1,34 @@
+function oabout() {
+  document.getElementById("about-page").style.width = "100%";
+}
+
+function cabout() {
+  document.getElementById("about-page").style.width = "0%";
+}
+
+
+function oprojects() {
+  document.getElementById("projects-page").style.width = "100%";
+}
+
+function cprojects() {
+  document.getElementById("projects-page").style.width = "0%";
+}
+
+
+function oportfolio() {
+  document.getElementById("portfolio-page").style.width = "100%";
+}
+
+function cportfolio() {
+  document.getElementById("portfolio-page").style.width = "0%";
+}
+
+
+function ocontact() {
+  document.getElementById("contact-page").style.width = "100%";
+}
+
+function cconact() {
+  document.getElementById("contact-page").style.width = "0%";
+}
